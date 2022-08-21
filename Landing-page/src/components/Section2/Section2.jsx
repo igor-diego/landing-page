1af0c2e77f8b6f5c3e089891011ece9a9e787514
Igor reis <igordiego237@gmail.com>
2022-08-20 21:53:20 -0300

    adicionei o componente Section2

---
+++ Landing-page/src/components/Section2/Section2.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { DivProduto } from '../DivProduto/DivProduto'
+import S from './Section2.module.css'
+
+export const Section2 = () => {
+  return (
+    <section className={S.Section2}>
+        <DivProduto />
+        <DivProduto />
+        <DivProduto />
+        <DivProduto />
+        <DivProduto />
+        <DivProduto />
+        <DivProduto />
+        <DivProduto />
+
+    </section>
+  )
+}
