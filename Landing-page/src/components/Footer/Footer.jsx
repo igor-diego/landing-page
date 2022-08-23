6c82c56b9fd964ebeb9013c7872c5631e9a83d40
Igor reis <igordiego237@gmail.com>
2022-08-22 21:21:42 -0300

    adicão do componente Footer

---
+++ Landing-page/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import S from './Footer.module.css'
+
+export const Footer = () => {
+  return (
+    <footer className={S.footer}>
+        <p className={S.p}>Testando suas habilidades em HTML, CSS e JS</p>
+        <p className={S.p}>linx inpulse</p>
+        <p className={S.p}>2019</p>
+    </footer>
+  )
+}
